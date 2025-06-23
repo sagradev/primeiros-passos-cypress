fdb80125ca93d4479b8a8391a273c27356182814
sagradev <mmpguty.5@gmail.com>
2025-06-23 19:32:02 -0400

    base url added

--- cypress/e2e/loginspec.cy.js
+++ cypress/e2e/loginspec.cy.js
@@ -1,4 +1,4 @@
-import userData from '../fixtures/user-data.json'
+import userData from '../fixtures/users/user-data.json'
 
 describe('Orange HRM Tests', () => {
 
@@ -11,9 +11,8 @@ describe('Orange HRM Tests', () => {
     dashboardGrid:".orangehrm-dashboard-grid"
   }
 
-
   it('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
@@ -21,7 +20,7 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorList.dashboardGrid)
 })
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userFail.username)
     cy.get(selectorList.passwordField).type(userData.userFail.password)
     cy.get(selectorList.loginButton).click()
